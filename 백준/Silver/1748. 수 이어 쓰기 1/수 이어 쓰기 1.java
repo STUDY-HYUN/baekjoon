0086847814b53f8f24d31d0e56b9b13979f8625b
hyun <70558461+hyunjungjeon@users.noreply.github.com>
2022-12-30 20:11:07 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 968 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 
@@ -12,7 +10,6 @@ public class Main {
 		// 이렇게 만들어진 새로운 수는 몇 자리 수일까? 이 수의 자릿수를 구하는 프로그램을 작성하시오.
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		
 		int cnt = 0; 
@@ -28,10 +25,7 @@ public class Main {
 			N--;
 		}
 		
-		bw.append(String.valueOf(cnt));
-        bw.flush();
-        bw.close();
-        br.close();
+		System.out.println(cnt);
 	}
 
 }
