bbfe9584efd0078d3fa1b3d9e0495b625f9ad8d7
hyun <70558461+hyunjungjeon@users.noreply.github.com>
2023-03-30 22:19:53 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,23 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringTokenizer st;
+	
+	private static int A,B,C;
+	
+	public static void main(String[] args) throws IOException {
+		/* (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
 
-	public static void main(String[] args) {
-		//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+		(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
 		
-		Scanner sc = new Scanner(System.in);
+		세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오. */
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
+		// 값 입력
+		st = new StringTokenizer(br.readLine());
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 		
 		System.out.println((A+B)%C);
 		System.out.println(((A%C) + (B%C))%C);
 		System.out.println((A*B)%C);
 		System.out.println(((A%C) * (B%C))%C);
 		
-		sc.close();
-
+		br.close();
 
 	}
 
