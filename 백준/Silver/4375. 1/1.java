94957f4ef429617c1b1f1a19260270b35c0f7b51
hyun <70558461+hyunjungjeon@users.noreply.github.com>
2023-03-30 23:34:33 +0900

    [Silver III] Title: 1, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,34 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-	private static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		
-		while(sc.hasNextInt()) {
-			int input = sc.nextInt();
-			
-			System.out.println(calculateMethod(input));
-		}
-		
-		sc.close();
-	}
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-	private static int calculateMethod(int input) {
-		
-		int onlyOneNumber = 1; //1로만 이루어진 n의 배수
-		
-		for(int digit = 1;; digit++) {
+	private static int n;
+	
+	public static void main(String[] args) throws IOException {
+		/* 2와 5로 나누어 떨어지지 않는 정수 n(1 ≤ n ≤ 10000)가 주어졌을 때, 1로만 이루어진 n의 배수를 찾는 프로그램을 작성하시오. */
+		String input;
+		// 정수 입력
+		while ((input = br.readLine()) != null) {
+			n = Integer.parseInt(input);
 			
-			if(onlyOneNumber % input == 0) {
-				return digit;
+			int digit = 1;
+			long num = 1;
+			
+			while(num % n != 0) {
+				num = ((num * 10) + 1)%n;
+				digit++;
 			}
-			
-			onlyOneNumber = (onlyOneNumber * 10 + 1) % input; // 1로 이루어진 n을 위한 연산
+		
+			System.out.println(digit);
 		}
 		
+		br.close();
 	}
 
 }
- 
