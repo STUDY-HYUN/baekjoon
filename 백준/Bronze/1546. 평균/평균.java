0508eb9b07094ca71cb274345ab4776763fffe0e
hyun <70558461+hyunjungjeon@users.noreply.github.com>
2023-04-14 20:58:59 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int M = 0;
+		int sum = 0;
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(num > M) M = num;
+			sum += num;
+		}
+	
+		
+		System.out.println(sum * 100.0 / M / N);
+		
+		br.close();
+		
+	}
+
+}
