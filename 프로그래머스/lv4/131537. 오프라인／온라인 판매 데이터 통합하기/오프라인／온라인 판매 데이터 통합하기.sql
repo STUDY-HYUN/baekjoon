68072cc865df5498fa9cbdadfb1ef6f71b526adb
hyun <70558461+hyunjungjeon@users.noreply.github.com>
2023-04-15 22:47:16 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,16 +1,20 @@
 -- 코드를 입력하세요
-SELECT
-       TO_CHAR(SALES_DATE, 'YYYY-mm-dd') as SALES_DATE
-     , PRODUCT_ID
-     , USER_ID
-     , SALES_AMOUNT
-  FROM ONLINE_SALE
- WHERE TO_CHAR(SALES_DATE, 'YYYY-mm-dd') LIKE '____-03-__'
-UNION
-SELECT
-        TO_CHAR(SALES_DATE, 'YYYY-mm-dd') as SALES_DATE
-     , PRODUCT_ID
-     , NULL AS USER_ID
-     , SALES_AMOUNT
-  FROM OFFLINE_SALE
- WHERE TO_CHAR(SALES_DATE, 'YYYY-mm-dd') LIKE '____-03-__'
+SELECT 
+       *
+  FROM (
+    SELECT
+           DATE_FORMAT(N.SALES_DATE, '%Y-%m-%d') as SALES_DATE
+         , N.PRODUCT_ID
+         , N.USER_ID
+         , N.SALES_AMOUNT
+      FROM ONLINE_SALE N
+     WHERE DATE_FORMAT(SALES_DATE, '%Y-%m-%d') LIKE '____-03-__'
+    UNION ALL
+    SELECT
+           DATE_FORMAT(F.SALES_DATE, '%Y-%m-%d') as SALES_DATE
+         , F.PRODUCT_ID
+         , NULL AS USER_ID
+         , F.SALES_AMOUNT
+      FROM OFFLINE_SALE F
+     WHERE DATE_FORMAT(F.SALES_DATE, '%Y-%m-%d') LIKE '____-03-__') T
+  ORDER BY T.SALES_DATE , T.PRODUCT_ID, T.USER_ID
